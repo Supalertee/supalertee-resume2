86e377e9baacde1efe089cc317728715eadbee40
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 02:50:16 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Resume",
         page_icon="👋",
     )
     
@@ -45,3 +45,138 @@ st.info('''
     - With a wealth of experience in both working and residing in diverse cultural environments.
     - test3.
     ''')
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://linkedin.com/in/sukrakarn" target="_blank">Supalert Sukrakarn</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work-experience">Work Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#hand-on-academic-projects"> Hand on Academic projects</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+#####################
+# Custom function for printing text
+def txt(a, b):
+  col1, col2 = st.columns([4,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+
+def txt2(a, b):
+  col1, col2 = st.columns([1,4])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+
+def txt3(a, b):
+  col1, col2 = st.columns([1,2])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  
+def txt4(a, b, c):
+  col1, col2, col3 = st.columns([1.5,2,2])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+  with col3:
+    st.markdown(c)
+
+
+#####################
+st.markdown('''
+## Education
+''')
+
+txt('**Master of Science** (Physics), *Hanyang University* , Republic of Korea',
+'2021-2023')
+st.markdown('''
+- GPA: `4.44`
+- Research thesis entitled `Symmetry Breaking Effect in Holographic Fermions `.
+- Received Korean Government Scholarship (GKS) covering tuition and stipend for 3 years.
+''')
+
+txt('**Bachelors of Science** (Physics), *Mahidol University*, Thailand',
+'2015-2019')
+st.markdown('''
+- GPA: `3.39`
+- Research thesis entitled `Perturbative Quantum Gravity with Application in Dark Matter Annihilation`.
+ - Best Senior Project Award, The 12th Senior Research Project Competition, organized by the Thai Physics Society.
+''')
+
+#####################
+st.markdown('''
+## Work Experience
+''')
+
+txt('**Full-Time Researcher**, String and Manybodys Research Group, Faculty of Natural Sciences, Hanyang University, Republic of Korea',
+'2023-2024')
+st.markdown('''
+- Conducted and published academic research that fulfills a way to understand solid-state physics which had been mysterious for  ` 20` years.
+- Played a key role in the success of the group's groundbreaking research project on the physics frontier of topology, which resulted in significant funding from the Korean government.
+- Optimized complex computations, an integrated approach was developed utilizing the programming languages `Wolfram Mathematica`, `Python`, and `Julia`. This resulted in a significant improvement, with numerical calculations being approximately  ` 1000` times faster than the original method.
+- Develop numerical/analytical calculations and machine learning to solve systems of non-linear `3 and more ` coupled differential equations.
+''')
+
+
+
+
+#####################
+st.markdown('''
+## Hand on Academic projects
+''')
+txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
+txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
+txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
+txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
+txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
+txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
+txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
+txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
+txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
+txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
+txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
+txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
+txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
+txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
+txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
+txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
+txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
+
+
+#####################
+st.markdown('''
+## Skills
+''')
+txt3('Programming', '`Python`, `R`, `Linux`')
+txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
+txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
+txt3('Machine Learning', '`scikit-learn`')
+txt3('Deep Learning', '`TensorFlow`')
+txt3('Web development', '`Flask`, `HTML`, `CSS`')
+txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+
+#####################
