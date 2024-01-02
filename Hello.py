01ce4f2e9e2609a40241359a2522df83c90e33bf
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 03:16:11 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -35,9 +35,7 @@ st.write('''
 ''')
 
 st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
+    G_{\mu\nu} = gT_{\mu\nu}
     ''')
 
 image = Image.open('image.png')
