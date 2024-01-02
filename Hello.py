23430348982fb7aea7af71c7576987309bcf6fce
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 03:15:13 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -33,6 +33,7 @@ st.write("# Welcome to My Portfolio  ! 👋")
 st.write('''
 # Supalert Sukrakarn, M.Sc. 
 ''')
+
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
