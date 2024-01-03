8e3c8c627a6d98e7756fc4b3fcdc4f666979527e
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 17:45:57 +0900

    Update 4_Academic_Research.py

--- pages/4_Academic_Research.py
+++ pages/4_Academic_Research.py
@@ -11,4 +11,6 @@ st.markdown(''' The main problem is that the gravity and quantum physics are bui
         unified theory. The unification is well known as `gauge/gravity duality`. The theories have been developing for 20s years, we now have elementary-particle\
             /gravity duality, fluid-dynamics/gravity duality, Condensed Matter Physics/gravity duality. All of them are simply called `Holographic Principle`''')
 st.markdown('''Holography is a principle that state that weakly interacting classical gravitational gravity in $n+1$ dimensions is equivelent to strongly coupling quantum theory.\
-    in $n$ dimensions. Now it is natural to be asked by people "why we need this". The answer is simple, we cannot solve strongly coupling quantum theoryaa''')
+    in $n$ dimensions. Now it is natural to be asked by people "why we need this". The answer is very simple, it is because we cannot \
+            solve strongly coupling quantum systems easily, while strongly coupling quantum systems have several extraordinary properties and features, which\
+                might become a part of next generation tenmologies. For example, superconductivity, Mott insulator, Weyl semimetals, flat-band, and so-on''')
