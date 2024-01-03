c9a4832555fc5638f3c54cc2b3b832f37d2a1b34
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 16:25:32 +0900

    Create 4_research.py

---
+++ pages/4_research.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown("# Physics Research Interests")
+st.sidebar.markdown("# Gravity/Condensed Matter Theorey Duality")
+
+st.markdown('''
+    In theoretical physics, there are a number of mysteriouse questions in gravity and elementary particle. Famous physicists like Einstien, Halking, Oppenheimer\
+         and so on also spent their entire life to unify gravity and elementary particle (quantum physics)''')
+st.markdown(''' The main problem is that the gravity and quantum physics are builded up by differences of fundamental. We now end-up with unification of theories\
+        unified theory. The unification is well known as `duality`''')
