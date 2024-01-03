29d5c7e198fcc6ea9677fe23b7aa481609fdaeef
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-01-03 16:35:31 +0900

    Update 4_research.py

--- pages/4_research.py
+++ pages/4_research.py
@@ -7,4 +7,6 @@ st.markdown('''
     In theoretical physics, there are a number of mysteriouse questions in gravity and elementary particle. Famous physicists like Einstien, Halking, Oppenheimer\
          and so on also spent their entire life to unify gravity and elementary particle (quantum physics)''')
 st.markdown(''' The main problem is that the gravity and quantum physics are builded up by differences of fundamental. We now end-up with unification of theories\
-        unified theory. The unification is well known as `duality`''')
+        unified theory. The unification is well known as `gauge/gravity duality`. The theories have been developing for 20s years, we now have elementary-particle\
+            /gravity duality, fluid-dynamics/gravity duality, Condensed Matter Physics/gravity duality. All of them are simply called `Holographic Principle`''')
+st.markdown(''' ''')
