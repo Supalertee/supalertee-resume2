684dd81e29f18040c1edfd74618d68866d3b421b
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-03-22 23:53:28 +0900

    long

--- pages/3_SQL.py
+++ pages/3_SQL.py
@@ -18,12 +18,14 @@ from sklearn.metrics import log_loss
 from sklearn.metrics import confusion_matrix, accuracy_score
 import sklearn.metrics as metrics
 from streamlit.hello.utils import show_code
+import sqlite3
 
+%load_ext sql
 
 zf1 = zipfile.ZipFile('./projects/SQL/Sales Data.zip') 
 Sales = pd.read_csv(zf1.open('Sales Data.csv'))
-
-st.dataframe(Sales.head(5))
-
+con = sqlite3.connect("SQLiteMagic.db")
+cur = con.cursor()
+Sales.to_sql("Sales", con, if_exists='replace', index=False,method="multi")
 
 
