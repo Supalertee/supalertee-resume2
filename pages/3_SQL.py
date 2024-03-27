7299d0dc4c5f7e89c4e743f897bc18bb595fad04
Supalertee <supalertee@gmail.com>
2024-03-28 01:03:06 +0900

    test

--- pages/3_SQL.py
+++ pages/3_SQL.py
@@ -1,31 +1,31 @@
-import streamlit as st
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+# import streamlit as st
+# import numpy as np
+# import pandas as pd 
+# import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
 
-import seaborn as sns
-import requests
-import geopandas as gpd
-import os
-import zipfile
-from pmdarima import auto_arima
-import time 
-from sklearn import preprocessing
-from sklearn.metrics import r2_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import log_loss
-from sklearn.metrics import confusion_matrix, accuracy_score
-import sklearn.metrics as metrics
-from streamlit.hello.utils import show_code
-import sqlite3
+# import seaborn as sns
+# import requests
+# import geopandas as gpd
+# import os
+# import zipfile
+# from pmdarima import auto_arima
+# import time 
+# from sklearn import preprocessing
+# from sklearn.metrics import r2_score
+# from sklearn.metrics import f1_score
+# from sklearn.metrics import log_loss
+# from sklearn.metrics import confusion_matrix, accuracy_score
+# import sklearn.metrics as metrics
+# from streamlit.hello.utils import show_code
+# import sqlite3
 
-%load_ext sql
+# %load_ext sql
 
-zf1 = zipfile.ZipFile('./projects/SQL/Sales Data.zip') 
-Sales = pd.read_csv(zf1.open('Sales Data.csv'))
-con = sqlite3.connect("SQLiteMagic.db")
-cur = con.cursor()
-Sales.to_sql("Sales", con, if_exists='replace', index=False,method="multi")
+# zf1 = zipfile.ZipFile('./projects/SQL/Sales Data.zip') 
+# Sales = pd.read_csv(zf1.open('Sales Data.csv'))
+# con = sqlite3.connect("SQLiteMagic.db")
+# cur = con.cursor()
+# Sales.to_sql("Sales", con, if_exists='replace', index=False,method="multi")
 
 
